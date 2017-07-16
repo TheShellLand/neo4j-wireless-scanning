702a4aa23f8bd6c3e03f0377bd2a261807b8d0ce
Eric Jaw <naisanza@gmail.com>
2017-07-16 19:18:10 -0400

    Added threading support starting services

--- start-me.py
+++ start-me.py
@@ -4,6 +4,12 @@
 """
 
 import iw.wireless
+import db.engine
+import threading
+
 
 if __name__ == '__main__':
-    iw.wireless.main()
+    threading.Thread(target=iw.wireless.main()).start()
+
+    # TODO: bug - Thread is blocking
+    threading.Thread(target=db.engine.main()).start()
